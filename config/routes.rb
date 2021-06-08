afe346150568831c722c616a60bd040820ed9912
Agnieszka Brown <abrown2012@icloud.com>
2021-06-08 12:24:41 -0400

    saves data just in case

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :answers
-  resources :questions
-  resources :quizzes
-  resources :users
+  resources :questions do
+    resources :answers
+  end 
+  resources :quizzes 
+
+  resources :users 
+
   resources :quiz_questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
